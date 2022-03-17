a9156ce144af4858fa9f816de62e0d315fa93d77
Quentin Gliech <quenting@element.io>
2022-03-17 14:59:25 +0100

    Add missing typing.List import

--- synapse/api/auth.py
+++ synapse/api/auth.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import logging
-from typing import TYPE_CHECKING, Optional, Tuple
+from typing import TYPE_CHECKING, List, Optional, Tuple
 from urllib.parse import urlencode
 
 import pymacaroons
